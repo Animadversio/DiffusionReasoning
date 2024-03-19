f5636e8dcef2117d22198a1bca309b6cace6bebc
Binxu Wang <binxu_wang@hms.harvard.edu>
2024-03-19 02:06:32 -0400

    add cli

--- eval_UNet_rules.sh
+++ eval_UNet_rules.sh
@@ -1,5 +1,6 @@
 
-exproot="/n/holylabs/LABS/kempner_fellows/Users/binxuwang/DL_Projects/mini_edm/exps"
+# exproot="/n/holylabs/LABS/kempner_fellows/Users/binxuwang/DL_Projects/mini_edm/exps"
+exproot="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/DL_Projects/mini_edm/exps"
 # 45,46,47,48,49,50,51,52,53,54,55,56
 # Assuming the experiment names follow the format "XXX-RAVEN10_abstract*"
 for expname in "${exproot}"/*X3_new_RAVEN10_abstract_*2024031* ; do
--- stats_plot_utils.py
+++ stats_plot_utils.py
@@ -1,7 +1,24 @@
 import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from os.path import join
+
+mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['axes.spines.right'] = False
+mpl.rcParams['axes.spines.top'] = False
+
+
+def saveallforms(figdirs, fignm, figh=None, fmts=("png", "pdf")):
+    """Save all forms of a figure in an array of directories."""
+    if type(figdirs) is str:
+        figdirs = [figdirs]
+    if figh is None:
+        figh = plt.gcf()
+    for figdir in figdirs:
+        for sfx in fmts:
+            figh.savefig(join(figdir, fignm+"."+sfx), bbox_inches='tight')
+
 
 def estimate_CI(count, total, alpha=0.05):
     """estimate the confidence interval of a ratio"""
