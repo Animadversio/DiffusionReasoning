8a262e4ae817263de5076e2658cf16b828118cc9
Binxu Wang <binxu_wang@hms.harvard.edu>
2024-04-15 20:30:22 -0400

    eval rules and memorization of EDM

--- eval_UNet_rules.sh
+++ eval_UNet_rules.sh
@@ -3,7 +3,8 @@
 exproot="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/DL_Projects/mini_edm/exps"
 # 45,46,47,48,49,50,51,52,53,54,55,56
 # Assuming the experiment names follow the format "XXX-RAVEN10_abstract*"
-for expname in "${exproot}"/*X3_new_RAVEN10_abstract_*2024031* ; do
+
+for expname in "${exproot}"/*RAVEN10_abstract_*202404* ; do
     encoding="--encoding digit"
     if [[ "$expname" == *"onehot"* ]]; then
         encoding="--encoding onehot"
@@ -13,3 +14,13 @@ for expname in "${exproot}"/*X3_new_RAVEN10_abstract_*2024031* ; do
         --exproot "$exproot" --figdir Figures_newrule \
         --expname "$(basename "$expname")" $encoding --update
 done
+# for expname in "${exproot}"/*X3_new_RAVEN10_abstract_*2024031* ; do
+#     encoding="--encoding digit"
+#     if [[ "$expname" == *"onehot"* ]]; then
+#         encoding="--encoding onehot"
+#     fi
+
+#     python edm_new_rule_check_CLI.py --ep_step 1000 --ep_stop 1200000 \
+#         --exproot "$exproot" --figdir Figures_newrule \
+#         --expname "$(basename "$expname")" $encoding --update
+# done
--- eval_memorization.sh
+++ eval_memorization.sh
@@ -1,18 +1,30 @@
 
-exproot="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/DL_Projects/DiT/results"
-# 45,46,47,48,49,50,51,52,53,54,55,56
+# exproot="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/DL_Projects/DiT/results"
+# # 45,46,47,48,49,50,51,52,53,54,55,56
+# # Assuming the experiment names follow the format "XXX-RAVEN10_abstract*"
+# for expname in "${exproot}"/0{61..80}-RAVEN10_abstract*; do
+#     encoding="--encoding digit"
+#     if [[ "$expname" == *"onehot"* ]]; then
+#         encoding="--encoding onehot"
+#     fi
+
+#     python check_memorization_CLI.py --ep_step 5000 --ep_start 1000 --ep_stop 1400000 \
+#         --fmt %07d.pt --exproot "$exproot" --figdir Figures_newrule \
+#         --expname "$(basename "$expname")" $encoding 
+# done
+
+exproot="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/DL_Projects/mini_edm/exps"
 # Assuming the experiment names follow the format "XXX-RAVEN10_abstract*"
-for expname in "${exproot}"/0{61..80}-RAVEN10_abstract*; do
+for expname in "${exproot}"/*RAVEN10_abstract_*202404* ; do
     encoding="--encoding digit"
     if [[ "$expname" == *"onehot"* ]]; then
         encoding="--encoding onehot"
     fi
 
-    python check_memorization_CLI.py --ep_step 5000 --ep_start 1000 --ep_stop 1400000 \
-        --fmt %07d.pt --exproot "$exproot" --figdir Figures_newrule \
+    python check_memorization_CLI.py --ep_step 5000 --ep_stop 1200000 \
+        --fmt tensor_%s.pt --exproot "$exproot" --figdir Figures_newrule \
         --expname "$(basename "$expname")" $encoding 
 done
-
 # # exproot="/n/holylabs/LABS/kempner_fellows/Users/binxuwang/DL_Projects/mini_edm/exps"
 # exproot="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/DL_Projects/mini_edm/exps"
 # # Assuming the experiment names follow the format "XXX-RAVEN10_abstract*"
