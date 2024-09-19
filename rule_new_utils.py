89207a89a6c46a70a6c11a7958ea91a30a413e93
Binxu Wang <binxu_wang@hms.harvard.edu>
2024-09-19 19:11:58 -0400

    refactor old analysis

--- rule_new_utils.py
+++ rule_new_utils.py
@@ -177,13 +177,15 @@ def infer_rule_from_sample_batch(sample_batch):
     return r3_list, r2_list, rule_col
 
 
-def compute_rule_statistics(r3_list, r2_list, rule_col):
-    r3_count = sum([len(x) > 0 for x in r3_list])
-    r2_count = sum([len(x) > 0 for x in r2_list])
+def compute_rule_statistics(r3_list, r2_list, rule_col, verbose=True):
+    C3_count = sum([len(x) > 0 for x in r3_list])
+    C2_count = sum([len(x) > 0 for x in r2_list])
     rule_flatten = np.array(rule_col, dtype=object).flatten() # [3 * 1024]
     anyvalid_count = sum([len(x) > 0 for x in rule_flatten])
     total = len(r3_list)
-    return r3_count, r2_count, anyvalid_count, total
+    if verbose:
+        print(f"C3: {C3_count}/{total} ({C3_count/total:.2f}), C3 + C2: {C3_count + C2_count}/{total} ({(C3_count + C2_count)/total:.2f}), AnyValid: {anyvalid_count}/{total*3} ({anyvalid_count/total/3:.2f})") 
+    return C3_count, C2_count, anyvalid_count, total
 
 
 def infer_rule_statistics_from_sample_batch(sample_batch):
