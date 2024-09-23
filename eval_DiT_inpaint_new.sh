e36ed76f9a5c88a163cace5fa47893e44be2d8fd
Binxu Wang <binxu_wang@hms.harvard.edu>
2024-09-23 12:08:08 -0400

    add new jobs for inpainting and synopsis

--- eval_DiT_inpaint_new.sh
+++ eval_DiT_inpaint_new.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
+# Test run with held out set. 
 exproot="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/DL_Projects/DiT/results"
 # figdir="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/Figures/DiffusionReasoning/repr_rule_classify"
-# 45,46,47,48,49,50,51,52,53,54,55,56
 # Assuming the experiment names follow the format "XXX-RAVEN10_abstract*"
 # for expname in "${exproot}"/0{90}-RAVEN10_abstract*; do
 for expname in "${exproot}"/090-RAVEN10_abstract*; do
@@ -22,6 +21,27 @@ for expname in "${exproot}"/090-RAVEN10_abstract*; do
     done
 done
 
+# Test run with full training set  
+exproot="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/DL_Projects/DiT/results"
+# Assuming the experiment names follow the format "XXX-RAVEN10_abstract*"
+for expname in "${exproot}"/085-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M*; do
+    echo $expname
+    python DiT_inpaint_tds_sampler_CLI.py \
+        --tds_batch_size 25  --sample_id_num 50  --sample_id_offset 50 \
+        --expname "$(basename "$expname")" \
+        --epoch 1000000
+
+    epochs=(20000 100000 200000 500000 700000 900000)
+    # Loop over each epoch value
+    for epoch in "${epochs[@]}"; do
+        python DiT_inpaint_tds_sampler_CLI.py \
+            --tds_batch_size 25  --sample_id_num 50  --sample_id_offset 50 \
+            --expname "$(basename "$expname")" \
+            --epoch "$epoch"
+    done
+done
+
+# Sweep runs with various experiments
 exproot="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/DL_Projects/DiT/results"
 for expname in "${exproot}"/{085..089}-RAVEN10_abstract*; do
     echo "Processing experiment: ${expname}"
@@ -29,15 +49,35 @@ for expname in "${exproot}"/{085..089}-RAVEN10_abstract*; do
         --tds_batch_size 25  --sample_id_num 50  --sample_id_offset 50 \
         --expname "$(basename "$expname")" \
         --epoch 1000000
-
 done
+
 for expname in "${exproot}"/{091..100}-RAVEN10_abstract*; do
     echo "Processing experiment: ${expname}"
     python DiT_inpaint_tds_sampler_CLI.py \
         --tds_batch_size 25  --sample_id_num 50  --sample_id_offset 50 \
         --expname "$(basename "$expname")" \
         --epoch 1000000
+done
 
+exproot="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/DL_Projects/DiT/results"
+for expname in "${exproot}"/{101..102}-RAVEN10_abstract*; do
+    echo "Processing experiment: ${expname}"
+    python DiT_inpaint_tds_sampler_CLI.py \
+        --tds_batch_size 25  --sample_id_num 50  --sample_id_offset 50 \
+        --expname "$(basename "$expname")" \
+        --epoch 1000000
+done
+
+for expname in "${exproot}"/{101..102}-RAVEN10_abstract*; do
+    echo $expname
+    epochs=(20000 100000 200000 500000 700000 900000)
+    # Loop over each epoch value
+    for epoch in "${epochs[@]}"; do
+        python DiT_inpaint_tds_sampler_CLI.py \
+            --tds_batch_size 25  --sample_id_num 50  --sample_id_offset 50 \
+            --expname "$(basename "$expname")" \
+            --epoch "$epoch"
+    done
 done
 
     # python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch -1 \
@@ -72,40 +112,3 @@ for expname in "${exproot}"/{085..089,091..100}-RAVEN10_abstract*; do
     echo "Processing experiment: $expname"
 done
 
-
-
-for expname in "${exproot}"/0{71,7}-RAVEN10_abstract*; do
-    encoding="--encoding digit"
-    if [[ "$expname" == *"onehot"* ]]; then
-        encoding="--encoding onehot"
-    fi
-
-    echo $expname
-    # python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch -1 \
-    #     --t_scalars 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 --PC_dim 1024 --figdir Figures_newrule
-    python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch 1000000 \
-        --t_scalars 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 --PC_dim 1024 --figdir Figures_newrule
-    python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch 20000 \
-        --t_scalars 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 --PC_dim 1024 --figdir Figures_newrule
-    # python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch 100000 \
-    #     --t_scalars 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 --PC_dim 1024 --figdir Figures_newrule
-    # python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch 200000 \
-    #     --t_scalars 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 --PC_dim 1024 --figdir Figures_newrule
-    # python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch 500000 \
-    #     --t_scalars 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 --PC_dim 1024 --figdir Figures_newrule
-
-    # python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch -1 \
-    #     --t_scalars 0.1 --noPCA --figdir Figures_newrule
-    python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch 1000000 \
-        --t_scalars 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 --noPCA --figdir Figures_newrule
-    python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch 20000 \
-        --t_scalars 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 --noPCA --figdir Figures_newrule
-    # python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch 100000 \
-    #     --t_scalars 0.1 --noPCA --figdir Figures_newrule
-    # python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch 200000 \
-    #     --t_scalars 0.1 --noPCA --figdir Figures_newrule
-    # python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch 500000 \
-    #     --t_scalars 0.1 --noPCA --figdir Figures_newrule
-    # python DiT_repr_classifier_new_probe_CLI.py.py --expname "$(basename "$expname")" --epoch 1000000 \
-    #     --t_scalars 0.1 --noPCA --figdir Figures_newrule
-done
