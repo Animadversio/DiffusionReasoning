95613d52ccf8d4963cb4ae2a95e5f9c0d2387e1d
Binxu Wang <binxu_wang@hms.harvard.edu>
2024-09-30 00:55:03 -0400

    add job

--- eval_DiT_uncond_sampler.sh
+++ eval_DiT_uncond_sampler.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 exproot="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/DL_Projects/DiT/results"
-figdir="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/Figures/DiffusionReasoning/uncond_sampler"
-# 45,46,47,48,49,50,51,52,53,54,55,56
+# figdir="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/Figures/DiffusionReasoning/uncond_sampler"
 # Assuming the experiment names follow the format "XXX-RAVEN10_abstract*"
 # for expname in "${exproot}"/0{90}-RAVEN10_abstract*; do
 for expname in "${exproot}"/{090,094,085,096}-RAVEN10_abstract*; do
@@ -11,39 +10,23 @@ for expname in "${exproot}"/{090,094,085,096}-RAVEN10_abstract*; do
         --epoch 1000000 --reps 10
 done
 
-
+exproot="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/DL_Projects/DiT/results"
+for expname in "${exproot}"/{086,087,088,089,091,092,093,095,097,098,101,102}-RAVEN10_abstract*; do
+    echo $expname
+    python DiT_uncond_sampler_sweep_CLI.py --expname "$(basename "$expname")" \
+        --epoch 1000000 --reps 10
+done
 
 
 exproot="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/DL_Projects/DiT/results"
-figdir="/n/holylfs06/LABS/kempner_fellow_binxuwang/Users/binxuwang/Figures/DiffusionReasoning/repr_rule_classify"
-
-for expname in "${exproot}"/098-RAVEN10_abstract-uncond-DiT_S_1-stream0_016M_heldout0*; do
-    encoding="--encoding digit"
-    if [[ "$expname" == *"onehot"* ]]; then
-        encoding="--encoding onehot"
-    fi
-
+for expname in "${exproot}"/{102,101,098,097,095,}-RAVEN10_abstract*; do
     echo $expname
-    python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch 1000000 --use_ema \
-        --t_scalars 0 1 10 25 50 100 250 500 1000 --dim_red_method avgtoken pca384 \
-        --layers 0 2 5 8 11 --figdir "$figdir"
-
-    epochs=(20000 100000 200000 500000 700000 900000)
-
-    # Loop over each epoch value
-    for epoch in "${epochs[@]}"; do
-        python DiT_repr_classifier_new_probe_CLI.py \
-            --expname "$(basename "$expname")" \
-            --epoch "$epoch" --use_ema \
-            --t_scalars 0 1 10 25 50 100 250 500 1000 \
-            --dim_red_method avgtoken pca384 \
-            --layers 0 2 5 8 11 \
-            --figdir "$figdir"
-    done
-
-    python DiT_repr_classifier_new_probe_CLI.py --expname "$(basename "$expname")" --epoch -1 \
-        --t_scalars 0 1 10 25 50 100 250 500 1000 --dim_red_method avgtoken pca384 \
-        --layers 0 2 5 8 11 --figdir "$figdir"
+    python DiT_uncond_sampler_sweep_CLI.py --expname "$(basename "$expname")" \
+        --epoch 1000000 --reps 10
 done
+
+
+
+
 
 
