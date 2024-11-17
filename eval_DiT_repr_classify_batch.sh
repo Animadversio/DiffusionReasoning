8627ef27bf0287e9cd61658cbdbdc8958785576c
Binxu Wang <binxu_wang@hms.harvard.edu>
2024-11-17 01:01:32 -0500

    add

--- eval_DiT_repr_classify_batch.sh
+++ eval_DiT_repr_classify_batch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -t 5:00:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 6:00:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p kempner_h100      # Partition to submit to
 #SBATCH -c 16               # Number of cores (-c)
-#SBATCH --mem=160G           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=150G           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --gres=gpu:1
-#SBATCH --array 1-12
+#SBATCH --array 13-24
 #SBATCH -o /n/home12/binxuwang/Github/DiffusionReasoning/cluster_log/DiT_repr_classify_%A_%a.out  
 #SBATCH -e /n/home12/binxuwang/Github/DiffusionReasoning/cluster_log/DiT_repr_classify_%A_%a.err  
 #SBATCH --mail-user=binxu_wang@hms.harvard.edu
@@ -24,6 +24,18 @@ param_list=\
 107-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel3_20241113-0056
 108-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel5_20241113-0057
 108-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel8_20241113-0057
+114-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel012_20241116-0330
+114-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel01234_20241116-0330
+114-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel56_20241116-0330
+115-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel014_20241116-0330
+116-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel1234_20241116-0331
+117-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel0123456_20241116-0331
+117-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel023_20241116-0331
+117-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel034_20241116-0331
+117-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel123_20241116-0331
+117-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel789_20241116-0331
+118-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel234_20241116-0331
+119-RAVEN10_abstract-uncond-DiT_S_1-stream0_16M_heldout_rel56789_20241116-0331
 "
 export param_name="$(echo "$param_list" | head -n $SLURM_ARRAY_TASK_ID | tail -1)"
 echo "$param_name"
